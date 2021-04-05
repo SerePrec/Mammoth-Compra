1e2b535548be4da2d5829771862d2ce963e0d77a
Sergio Prellezo <prellezose@yahoo.com.ar>
2021-04-05 16:36:43 -0300

    Se agregaron más productos (total: 105) y se hizo esquema inicial de filtros

--- js/objetosProducto.js
+++ js/objetosProducto.js
@@ -376,8 +376,8 @@ productos.push(new Producto(
     "Sram",
     "Freno Sram Code RSC A1 Delantero",
     25400,
-    12,
-    3,
+    14,
+    0,
     1,
     "producto29.jpg",
     false
@@ -389,7 +389,7 @@ productos.push(new Producto(
     "Sram",
     "Freno Sram Code RSC A1 Trasero",
     25800,
-    12,
+    14,
     3,
     1,
     "producto30.jpg",
@@ -610,7 +610,7 @@ productos.push(new Producto(
     "BNB Rack",
     "Barras Porta BNB Rack 2 biker 4x4",
     55000,
-    15,
+    16,
     4,
     1,
     "producto47.jpg",
@@ -668,3 +668,706 @@ productos.push(new Producto(
     "producto50.jpg",
     true
 ));
+productos.push(new Producto(
+    "52",
+    "accesorios",
+    "CODIGO",
+    "Sram",
+    "Cadena Sram PCX! 118 Powerlock 11V",
+    2850,
+    0,
+    3,
+    1,
+    "producto52.jpg",
+    false
+));
+productos.push(new Producto(
+    "53",
+    "accesorios",
+    "CODIGO",
+    "Shimano",
+    "Cadena Shimano Deore CN-M6100 12V",
+    3260,
+    0,
+    0,
+    1,
+    "producto53.jpg",
+    false
+));
+productos.push(new Producto(
+    "54",
+    "accesorios",
+    "CODIGO",
+    "Topeak",
+    "Corta Cadena Topeak Super",
+    1240,
+    18,
+    5,
+    2,
+    "producto54.jpg",
+    false
+));
+productos.push(new Producto(
+    "55",
+    "accesorios",
+    "CODIGO",
+    "Topeak",
+    "Multiherramienta Topeak Survival Gearbox",
+    2500,
+    0,
+    5,
+    2,
+    "producto55.jpg",
+    true
+));
+productos.push(new Producto(
+    "56",
+    "accesorios",
+    "CODIGO",
+    "Crankbrothers",
+    "Multiherramienta Crankbrothers M-19",
+    2900,
+    0,
+    5,
+    1,
+    "producto56.jpg",
+    false
+));
+productos.push(new Producto(
+    "57",
+    "accesorios",
+    "CODIGO",
+    "Sigma",
+    "Juego de luces Sigma Aura 80 + Blaze",
+    6700,
+    15,
+    5,
+    1,
+    "producto57.jpg",
+    false
+));
+productos.push(new Producto(
+    "58",
+    "equipamiento",
+    "CODIGO",
+    "Specialized",
+    "Casco Specialized Chamonix Mips",
+    7230,
+    0,
+    4,
+    1,
+    "producto58.jpg",
+    false
+));
+productos.push(new Producto(
+    "59",
+    "equipamiento",
+    "CODIGO",
+    "Giro",
+    "Casco Giro Agilis",
+    6700,
+    12,
+    3,
+    1,
+    "producto59.jpg",
+    false
+));
+productos.push(new Producto(
+    "60",
+    "equipamiento",
+    "CODIGO",
+    "Cube",
+    "Casco Cube AM Race",
+    5960,
+    0,
+    5,
+    1,
+    "producto60.jpg",
+    true
+));
+productos.push(new Producto(
+    "61",
+    "equipamiento",
+    "CODIGO",
+    "Bell",
+    "Casco Bell Avenue",
+    5030,
+    14,
+    0,
+    1,
+    "producto61.jpg",
+    false
+));
+productos.push(new Producto(
+    "62",
+    "equipamiento",
+    "CODIGO",
+    "Oakley",
+    "Lentes Oakley Flight Jacket Matte Black",
+    14300,
+    0,
+    4,
+    1,
+    "producto62.jpg",
+    true
+));
+productos.push(new Producto(
+    "63",
+    "equipamiento",
+    "CODIGO",
+    "Oakley",
+    "Lentes Oakley Radar EV Path Steel Fotocromático",
+    15900,
+    0,
+    4,
+    1,
+    "producto63.jpg",
+    true
+));
+productos.push(new Producto(
+    "64",
+    "equipamiento",
+    "CODIGO",
+    "Spiuk",
+    "Lentes Spiuk Jifter Espejo",
+    4450,
+    14,
+    5,
+    1,
+    "producto64.jpg",
+    false
+));
+productos.push(new Producto(
+    "65",
+    "equipamiento",
+    "CODIGO",
+    "Eltin",
+    "Lentes Eltin Full Oversize",
+    3990,
+    10,
+    5,
+    1,
+    "producto65.jpg",
+    false
+));
+productos.push(new Producto(
+    "66",
+    "equipamiento",
+    "CODIGO",
+    "Fox",
+    "Protector Coraza Fox Head Raceframe",
+    12500,
+    17,
+    4,
+    1,
+    "producto66.jpg",
+    true
+));
+productos.push(new Producto(
+    "67",
+    "equipamiento",
+    "CODIGO",
+    "Alpinestars",
+    "Protector Rodillera Alpinestars E-Ride Knee",
+    6700,
+    0,
+    4,
+    1,
+    "producto67.jpg",
+    false
+));
+productos.push(new Producto(
+    "68",
+    "equipamiento",
+    "CODIGO",
+    "Camelback",
+    "Mochila Camelback Mini Mule 1.5L",
+    4600,
+    0,
+    4,
+    1,
+    "producto68.jpg",
+    true
+));
+productos.push(new Producto(
+    "69",
+    "equipamiento",
+    "CODIGO",
+    "Ergon",
+    "Mochila Ergon BE2 Enduro",
+    12400,
+    19,
+    4,
+    1,
+    "producto69.jpg",
+    false
+));
+productos.push(new Producto(
+    "70",
+    "equipamiento",
+    "CODIGO",
+    "Camelback",
+    "Mochila Camelback Hydroback Light 1.5L",
+    4360,
+    22,
+    5,
+    1,
+    "producto70.jpg",
+    false
+));
+productos.push(new Producto(
+    "71",
+    "equipamiento",
+    "CODIGO",
+    "Ergon",
+    "Mochila Ergon BE3 Enduro",
+    13590,
+    12,
+    5,
+    1,
+    "producto71.jpg",
+    true
+));
+productos.push(new Producto(
+    "72",
+    "equipamiento",
+    "CODIGO",
+    "Alpinestars",
+    "Protector Codera Alpinestars E-Vent",
+    6420,
+    10,
+    3,
+    1,
+    "producto72.jpg",
+    false
+));
+productos.push(new Producto(
+    "73",
+    "equipamiento",
+    "CODIGO",
+    "Endura",
+    "Protector Rodillera Endura MT500 Lite",
+    6340,
+    9,
+    3,
+    1,
+    "producto73.jpg",
+    false
+));
+productos.push(new Producto(
+    "74",
+    "indumentaria",
+    "CODIGO",
+    "Castelli",
+    "Remera Castelli Competizione Ineos Grenadiers",
+    7995,
+    0,
+    5,
+    1,
+    "producto74.jpg",
+    true
+));
+productos.push(new Producto(
+    "75",
+    "indumentaria",
+    "CODIGO",
+    "Cube",
+    "Remera Cube TeamLine L/S",
+    5490,
+    23,
+    5,
+    1,
+    "producto75.jpg",
+    true
+));
+productos.push(new Producto(
+    "76",
+    "indumentaria",
+    "CODIGO",
+    "Ale",
+    "Remera Ale Movistar 2020 Prime",
+    6950,
+    18,
+    5,
+    1,
+    "producto76.jpg",
+    false
+));
+productos.push(new Producto(
+    "77",
+    "indumentaria",
+    "CODIGO",
+    "Kimoa",
+    "Remera Kimoa Lab 01",
+    8990,
+    0,
+    0,
+    1,
+    "producto77.jpg",
+    false
+));
+productos.push(new Producto(
+    "78",
+    "indumentaria",
+    "CODIGO",
+    "Giant",
+    "Remera Giant Ride Like King",
+    5990,
+    20,
+    4,
+    1,
+    "producto78.jpg",
+    false
+));
+productos.push(new Producto(
+    "79",
+    "indumentaria",
+    "CODIGO",
+    "Specialized",
+    "Remera Specialized Roubaix Comp W",
+    4490,
+    16,
+    3,
+    1,
+    "producto79.jpg",
+    true
+));
+productos.push(new Producto(
+    "80",
+    "indumentaria",
+    "CODIGO",
+    "Cube",
+    "Remera Cube WLS TeamLine W",
+    4340,
+    16,
+    3,
+    1,
+    "producto80.jpg",
+    false
+));
+productos.push(new Producto(
+    "81",
+    "indumentaria",
+    "CODIGO",
+    "Orbea",
+    "Maillot Orbea Skinsuit Lab",
+    18340,
+    0,
+    3,
+    1,
+    "producto81.jpg",
+    false
+));
+productos.push(new Producto(
+    "82",
+    "indumentaria",
+    "CODIGO",
+    "Castelli",
+    "Maillot Castelli Integral Free Sanremo 2",
+    17980,
+    0,
+    3,
+    1,
+    "producto82.jpg",
+    false
+));
+productos.push(new Producto(
+    "83",
+    "indumentaria",
+    "CODIGO",
+    "Spiuk",
+    "Maillot Spiuk Tritraje Universal W",
+    8000,
+    0,
+    3,
+    1,
+    "producto83.jpg",
+    true
+));
+productos.push(new Producto(
+    "84",
+    "indumentaria",
+    "CODIGO",
+    "Gobik",
+    "Calza Gobik Absolute 4.0 K10",
+    10000,
+    0,
+    3,
+    1,
+    "producto84.jpg",
+    true
+));
+productos.push(new Producto(
+    "85",
+    "indumentaria",
+    "CODIGO",
+    "Specialized",
+    "Calza Specialized Rbx Sport Logo",
+    6690,
+    24,
+    4,
+    1,
+    "producto85.jpg",
+    false
+));
+productos.push(new Producto(
+    "86",
+    "indumentaria",
+    "CODIGO",
+    "Ale",
+    "Calza Ale Movistar 2020 Prime",
+    8090,
+    12,
+    4,
+    1,
+    "producto86.jpg",
+    false
+));
+productos.push(new Producto(
+    "87",
+    "indumentaria",
+    "CODIGO",
+    "Sportful",
+    "Calza Sportful GTS Short",
+    7680,
+    10,
+    4,
+    1,
+    "producto87.jpg",
+    false
+));
+productos.push(new Producto(
+    "88",
+    "indumentaria",
+    "CODIGO",
+    "Cube",
+    "Calza Cube WLS TeamLine W",
+    4950,
+    0,
+    4,
+    1,
+    "producto88.jpg",
+    true
+));
+productos.push(new Producto(
+    "89",
+    "indumentaria",
+    "CODIGO",
+    "Endura",
+    "Calza Endura FS260 Pro DS W",
+    9460,
+    27,
+    4,
+    1,
+    "producto89.jpg",
+    false
+));
+productos.push(new Producto(
+    "90",
+    "indumentaria",
+    "CODIGO",
+    "Scott",
+    "Calza Scott Bike RC Pro W",
+    7590,
+    0,
+    3,
+    1,
+    "producto90.jpg",
+    true
+));
+productos.push(new Producto(
+    "91",
+    "indumentaria",
+    "CODIGO",
+    "Sportful",
+    "Campera Sportful Reflex",
+    3770,
+    25,
+    4,
+    1,
+    "producto91.jpg",
+    true
+));
+productos.push(new Producto(
+    "92",
+    "indumentaria",
+    "CODIGO",
+    "Sportful",
+    "Campera Sportful Hot Pack 6",
+    6280,
+    0,
+    5,
+    1,
+    "producto92.jpg",
+    false
+));
+productos.push(new Producto(
+    "93",
+    "indumentaria",
+    "CODIGO",
+    "Cube",
+    "Campera chaleco Cube TeamLine",
+    3850,
+    15,
+    5,
+    1,
+    "producto93.jpg",
+    false
+));
+productos.push(new Producto(
+    "94",
+    "indumentaria",
+    "CODIGO",
+    "Castelli",
+    "Campera chaleco Castelli Aria Vest",
+    8495,
+    0,
+    5,
+    1,
+    "producto94.jpg",
+    true
+));
+productos.push(new Producto(
+    "95",
+    "indumentaria",
+    "CODIGO",
+    "Endura",
+    "Campera Endura FS260-Pro Adenaline Race Cape W",
+    7200,
+    0,
+    5,
+    1,
+    "producto95.jpg",
+    false
+));
+productos.push(new Producto(
+    "96",
+    "indumentaria",
+    "CODIGO",
+    "Gore",
+    "Campera Gore C5 Gore-Text Active W",
+    16800,
+    0,
+    5,
+    1,
+    "producto96.jpg",
+    true
+));
+productos.push(new Producto(
+    "97",
+    "indumentaria",
+    "CODIGO",
+    "Endura",
+    "Guantes Endura Windchill W",
+    2880,
+    20,
+    5,
+    1,
+    "producto97.jpg",
+    false
+));
+productos.push(new Producto(
+    "98",
+    "indumentaria",
+    "CODIGO",
+    "Specialized",
+    "Guantes Specialized BG Grail W",
+    2380,
+    0,
+    5,
+    1,
+    "producto98.jpg",
+    true
+));
+productos.push(new Producto(
+    "99",
+    "indumentaria",
+    "CODIGO",
+    "Specialized",
+    "Guantes Specialized BG Sport Gel SF",
+    2590,
+    0,
+    5,
+    1,
+    "producto99.jpg",
+    false
+));
+productos.push(new Producto(
+    "100",
+    "indumentaria",
+    "CODIGO",
+    "Endura",
+    "Guantes Endura Hummvee Plus 2",
+    2550,
+    0,
+    5,
+    1,
+    "producto100.jpg",
+    true
+));
+productos.push(new Producto(
+    "101",
+    "indumentaria",
+    "CODIGO",
+    "Specialized",
+    "Zapatillas Specialized Torch 3.0 Road",
+    22990,
+    0,
+    5,
+    1,
+    "producto101.jpg",
+    true
+));
+productos.push(new Producto(
+    "102",
+    "indumentaria",
+    "CODIGO",
+    "Sidi",
+    "Zapatillas Sidi Shot Carbon Matt",
+    24890,
+    0,
+    5,
+    1,
+    "producto102.jpg",
+    false
+));
+productos.push(new Producto(
+    "103",
+    "indumentaria",
+    "CODIGO",
+    "Shimano",
+    "Zapatillas Shimano RC500",
+    11890,
+    12,
+    6,
+    1,
+    "producto103.jpg",
+    false
+));
+productos.push(new Producto(
+    "104",
+    "indumentaria",
+    "CODIGO",
+    "Scott",
+    "Zapatillas Scott MTB RC W",
+    22420,
+    0,
+    5,
+    1,
+    "producto104.jpg",
+    true
+));
+productos.push(new Producto(
+    "105",
+    "indumentaria",
+    "CODIGO",
+    "Giro",
+    "Zapatillas Giro Cylinder 2 W",
+    12900,
+    13,
+    5,
+    1,
+    "producto104.jpg",
+    false
+));
+
